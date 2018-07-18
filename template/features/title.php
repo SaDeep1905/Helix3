727e821dacfa013898b97f66ccc5eb3bfd541332
Rifat Wahid Alif <rifat3280@gmail.com>
2018-07-18 13:05:23 +0600

    - Changed year 2018 in all files

--- template/features/title.php
+++ template/features/title.php
@@ -2,9 +2,10 @@
 /**
  * @package Helix3 Framework
  * @author JoomShaper http://www.joomshaper.com
- * @copyright Copyright (c) 2010 - 2014 JoomShaper
+ * @copyright Copyright (c) 2010 - 2018 JoomShaper
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
 */
+
 //no direct accees
 defined ('_JEXEC') or die('resticted aceess');
 
