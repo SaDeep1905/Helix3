f66f0e08c57274575bc0e17919eb82e57a2c241f
Rifat Wahid Alif <rifat3280@gmail.com>
2018-07-19 17:12:55 +0600

    - Article email send backward compatibility added.

--- template/html/com_mailto/mailto/default.php
+++ template/html/com_mailto/mailto/default.php
@@ -27,13 +27,15 @@ JFactory::getDocument()->addScriptDeclaration("
 		form.submit();
 	}
 ");
+
 ?>
 
 <div id="mailto-window">
 	<h2>
 		<?php echo JText::_('COM_MAILTO_EMAIL_TO_A_FRIEND'); ?>
 	</h2>
-
+	
+	<?php if(version_compare(JVERSION, '3.8.8', 'ge')) { ?>
 	<form action="<?php echo JRoute::_('index.php?option=com_mailto&task=send'); ?>" method="post" class="form-validate">
 		<fieldset>
 			<?php foreach ($this->form->getFieldset('') as $field) : ?>
@@ -59,4 +61,48 @@ JFactory::getDocument()->addScriptDeclaration("
 		<input type="hidden" name="link" value="<?php echo $this->link; ?>" />
 		<?php echo JHtml::_('form.token'); ?>
 	</form>
+	<?php } else {?>
+		<form action="<?php echo JUri::base() ?>index.php" id="mailtoForm" method="post">
+
+			<div class="control-group">
+				<label for="mailto_field" class="control-label"><?php echo JText::_('COM_MAILTO_SENDER'); ?></label>
+				<div class="controls">
+					<input type="text" id="mailto_field" name="mailto" value="<?php echo $this->escape($data->mailto); ?>">
+				</div>
+			</div>
+
+			<div class="control-group">
+				<label for="sender_field" class="control-label"><?php echo JText::_('COM_MAILTO_EMAIL_TO'); ?></label>
+				<div class="controls">
+					<input type="text" id="sender_field" name="sender" value="<?php echo $this->escape($data->sender); ?>">
+				</div>
+			</div>
+
+			<div class="control-group">
+				<label for="from_field" class="control-label"><?php echo JText::_('COM_MAILTO_YOUR_EMAIL'); ?></label>
+				<input type="text" id="from_field" name="from" value="<?php echo $this->escape($data->from); ?>">
+			</div>
+
+			<div class="control-group">
+				<label for="subject_field" class="control-label"><?php echo JText::_('COM_MAILTO_SUBJECT'); ?></label>
+				<input type="text" id="subject_field" name="subject" value="<?php echo $this->escape($data->subject); ?>">
+			</div>
+
+			<div class="control-group">
+				<button class="btn btn-success" onclick="return Joomla.submitbutton('send');">
+					<?php echo JText::_('COM_MAILTO_SEND'); ?>
+				</button>
+				<button class="btn btn-danger" onclick="window.close();return false;">
+					<?php echo JText::_('COM_MAILTO_CANCEL'); ?>
+				</button>
+			</div>
+
+			<input type="hidden" name="layout" value="<?php echo $this->getLayout();?>" />
+			<input type="hidden" name="option" value="com_mailto" />
+			<input type="hidden" name="task" value="send" />
+			<input type="hidden" name="tmpl" value="component" />
+			<input type="hidden" name="link" value="<?php echo $data->link; ?>" />
+			<?php echo JHtml::_('form.token'); ?>
+		</form>
+	<?php } ?>
 </div>
